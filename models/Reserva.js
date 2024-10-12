efd94b86787b499404575f8961840620fdbc5ca6
Ignacio Sekhard <nachostifler@gmail.com>
2024-10-12 20:41:05 -0300

    Cambios en el Back

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,34 +1,34 @@
 // models/Reserva.js - Modelo de Reserva
 class Reserva {
-    static async obtenerTodas() {
-      const { data, error } = await supabase.from('Reservas').select('*')
-      if (error) throw error
-      return data
-    }
-  
-    static async obtenerPorId(id) {
-      const { data, error } = await supabase.from('Reservas').select('*').eq('ID_Reserva', id).single()
-      if (error) throw error
-      return data
-    }
-  
-    static async crearReserva(reserva) {
-      const { data, error } = await supabase.from('Reservas').insert([reserva]).select('*')
-      if (error) throw error
-      return data[0]
-    }
-  
-    static async actualizarReserva(id, data) {
-      const { data: updatedData, error } = await supabase.from('Reservas').update(data).eq('ID_Reserva', id).select('*')
-      if (error) throw error
-      return updatedData[0]
-    }
-  
-    static async eliminarReserva(id) {
-      const { data, error } = await supabase.from('Reservas').delete().eq('ID_Reserva', id).select('*')
-      if (error) throw error
-      return data[0]
-    }
+  static async obtenerTodas() {
+    const { data, error } = await supabase.from('"reservas"').select('*')
+    if (error) throw error
+    return data
   }
-  
-  module.exports = Reserva
+
+  static async obtenerPorId(id) {
+    const { data, error } = await supabase.from('"reservas"').select('*').eq('"id_reserva"', id).single()
+    if (error) throw error
+    return data
+  }
+
+  static async crearReserva(reserva) {
+    const { data, error } = await supabase.from('"reservas"').insert([reserva]).select('*')
+    if (error) throw error
+    return data[0]
+  }
+
+  static async actualizarReserva(id, data) {
+    const { data: updatedData, error } = await supabase.from('"reservas"').update(data).eq('"id_reserva"', id).select('*')
+    if (error) throw error
+    return updatedData[0]
+  }
+
+  static async eliminarReserva(id) {
+    const { data, error } = await supabase.from('"reservas"').delete().eq('"id_reserva"', id).select('*')
+    if (error) throw error
+    return data[0]
+  }
+}
+
+module.exports = Reserva
