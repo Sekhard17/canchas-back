2dc9aeb8bff443de2fb9be6795c8b3bb61d77fd7
Ignacio Sekhard <nachostifler@gmail.com>
2024-10-13 03:07:23 -0300

    cambio en tabla Pagos

--- models/Pago.js
+++ models/Pago.js
@@ -1,34 +1,34 @@
 // models/Pago.js - Modelo de Pago
 class Pago {
-    static async obtenerTodos() {
-      const { data, error } = await supabase.from('Pagos').select('*')
-      if (error) throw error
-      return data
-    }
-  
-    static async obtenerPorId(id) {
-      const { data, error } = await supabase.from('Pagos').select('*').eq('ID_Pago', id).single()
-      if (error) throw error
-      return data
-    }
-  
-    static async crearPago(pago) {
-      const { data, error } = await supabase.from('Pagos').insert([pago]).select('*')
-      if (error) throw error
-      return data[0]
-    }
-  
-    static async actualizarPago(id, data) {
-      const { data: updatedData, error } = await supabase.from('Pagos').update(data).eq('ID_Pago', id).select('*')
-      if (error) throw error
-      return updatedData[0]
-    }
-  
-    static async eliminarPago(id) {
-      const { data, error } = await supabase.from('Pagos').delete().eq('ID_Pago', id).select('*')
-      if (error) throw error
-      return data[0]
-    }
+  static async obtenerTodos() {
+    const { data, error } = await supabase.from('pagos').select('*') // "pagos" en minúsculas
+    if (error) throw error
+    return data
   }
-  
-  module.exports = Pago
+
+  static async obtenerPorId(id) {
+    const { data, error } = await supabase.from('pagos').select('*').eq('id_pago', id).single() // "id_pago" en minúsculas
+    if (error) throw error
+    return data
+  }
+
+  static async crearPago(pago) {
+    const { data, error } = await supabase.from('pagos').insert([pago]).select('*') // "pagos" en minúsculas
+    if (error) throw error
+    return data[0]
+  }
+
+  static async actualizarPago(id, data) {
+    const { data: updatedData, error } = await supabase.from('pagos').update(data).eq('id_pago', id).select('*') // "id_pago" en minúsculas
+    if (error) throw error
+    return updatedData[0]
+  }
+
+  static async eliminarPago(id) {
+    const { data, error } = await supabase.from('pagos').delete().eq('id_pago', id).select('*') // "id_pago" en minúsculas
+    if (error) throw error
+    return data[0]
+  }
+}
+
+module.exports = Pago
